c843021247c1fadb3c9896361db8971e54a57aab
Bhattonjones <adroitprograming@gmail.com>
2021-01-05 13:37:13 +1000

    - Two scripts to get the hand and colliders to get "Gripped"

---
+++ Scripts/GripToHand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Method finds the right or left hand, however it is instantiated and then it attaches itself onto the hand object.
+/// -------------------------------------------------
+/// Bradley HJ - 5 Jan 2021
+/// </summary>
+public class GripToHand : MonoBehaviour
+{
+    public string NameOfHand; //Insert the string of the object that the hand is associated with.
+    public bool HandObtained; //If the hand is found, attach yourself to it. Set as parent and follow the transform.
+    private Transform Hand; //The identified hand and its Transform.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("FindHand", 0, 1f); //Every second try and find the hand.
+    }
+
+
+    private void FindHand() {
+        if (!HandObtained) { 
+        
+        }
+    }
+}
---
+++ Scripts/PickupObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Method finds the collider object in the hand to get picked up.
+/// This method lives on the object that can be picked up.
+/// -------------------------------------------------
+/// Bradley HJ - 5 Jan 2021
+/// </summary>
+public class PickupObject : MonoBehaviour
+{
+    public string NameOfObjectsHand; //Name of the object that is moving to the hand model
+    public string NameOfTable; // Name of the table that the object will be placed upon.
+    public bool IsObjectInUse; //Are you currently using the object in your hand.
+
+    private GameObject Hand;
+    private GameObject Table;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Check the colliders name, if the object is not in use then it wants to check if the hand is the collider, if the object is in use then it should look for the table.
+    /// </summary>
+    /// <param name="other">This a collision with an object that has a collider.</param>
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+
+    //Go to the hand. 
+    private void ToHand() { 
+    
+    }
+
+    //Go to the table.
+    private void ToTable() { 
+    
+    }
+}
