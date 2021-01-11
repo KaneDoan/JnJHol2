4fb6d826b35f598902bddd8d945fe2f9c8d0c45a
Bhattonjones <adroitprograming@gmail.com>
2021-01-11 15:26:40 +1000

    # Added ability to grab onto some objects and click them into place. # Fixed up the models faces to show better results # Added a human model for future additions # New script SnapToObject -> Simple collision and event script.

---
+++ Scripts/SnapToObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SnapToObject : MonoBehaviour
+{
+    public string ObjectINeedToSnap;
+    //public Vector3 LocalCo_OrdToSnap;
+
+    public UnityEvent IfObjectSnapped;
+
+    private void OnTriggerEnter(Collider other) {
+        string otherName = other.name;
+        
+        if (ObjectINeedToSnap == otherName) {
+            ///---- Commented code in for learning. Could not snap and then disable the object manipulator script to allow the object to move away from the persons hand.
+            ///Instead, until we can learn otherwise. I am disabling the object entirely and enabling a ghosted object which is not interactable.
+
+            //other.transform.parent = gameObject.transform;
+            //other.GetComponent<BoxCollider>().enabled = false;
+            //other.GetComponent<Rigidbody>().detectCollisions = false;
+            //other.transform.localPosition = LocalCo_OrdToSnap;
+            //Cannot disable ObjectManipulator.
+
+            //Turn off the object for now.
+            other.gameObject.SetActive(false);
+
+            IfObjectSnapped.Invoke();
+        }
+    }
+}
